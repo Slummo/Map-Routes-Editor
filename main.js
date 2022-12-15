a36133e5b9d1bb7bb34d2a158c1a75d251f10201
Narr0wB <iliash4@hotmail.com>
2022-12-15 09:47:56 +0100

    linking backend with frontend

---
+++ main.js
@@ -0,0 +1,38 @@
+//index.js
+const testAddon = require('./backend/build/Release/addon')
+const path = require('path')
+const { ipcMain, BrowserWindow, app, screen } = require('electron');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const createWindow = () => {
+    const win = new BrowserWindow({width: 800, height: 600, autoHideMenuBar: true, webPreferences: {
+        preload: path.join(__dirname, 'backend/preload.js')
+    }, contextIsolation: true})
+
+    win.loadFile("frontend/index.html")
+}
+
+app.whenReady().then(() => {createWindow();})
+
+// var eddu
+
+// testAddon.startEngine(800, 600);
+// eddu = testAddon.loadImage();
+
+// async function ed() {
+//     await sleep(3000);
+//     console.log(testAddon.loadFrame())
+// } 
+
+// ed()
+
+// ipcMain.on("debug", function(event, data) {console.log(data)})
+
+
+// ipcMain.handle("image_request", async (event, arg) => {
+//     return eddu;
+// })
+
